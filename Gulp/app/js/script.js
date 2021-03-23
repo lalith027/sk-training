716f746522630db87571d4c2e15857f24dee22e9
Lalit <lalith027@gmail.com>
2021-03-23 12:48:43 +0200

    new

---
+++ Gulp/app/js/script.js
@@ -0,0 +1,62 @@
+// let a = 0;
+// let b = 2;
+// let c = 0;
+
+// a = prompt("Input your digit", "0");
+
+// if (a != null) {
+
+//   console.log("ok was pressed");
+//   a = parseInt(a);
+//   if (a > 0 && a < 10 ) {
+//     c = a + b * 3;
+//   }  else {
+//     //a==0
+//     c = a * 2 + b * 4;
+//   }
+// } else {
+//   console.log("cancel was pressed");
+// }
+
+// let d = (a>0)? a+b:a*b;
+// alert(c);
+
+// alert(c);
+// c++;
+// console.log(c);
+
+
+// a = prompt("Input your digit", "0");
+
+// if (a > 0 || a < 0 ) {
+//        console.log(1);
+//      }  else {
+//        //a==0
+    //  console.log(0);
+
+    //  let d = (a > 0) ? 1:0;
+
+    //  console.log(d);
+
+
+// a = +prompt("Input your digit", "0");
+// switch (a) {
+// case 1:
+// alert ('a');
+// break;
+// case 2:
+// alert ('b');
+// break;
+// case 3:
+// alert ('c');
+// break;
+// default:
+// alert ("z");
+// }
+
+for(let i=9; i>1; i--) {
+  
+  console.log(i**2);}
+  console.log(n);
+
+  
