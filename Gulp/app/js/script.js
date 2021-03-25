47141f77a4b0e868d225dba6341b7c352c6a82d1
Lalit <lalith027@gmail.com>
2021-03-25 21:13:49 +0200

    new

---
+++ Gulp/app/js/script.js
@@ -0,0 +1,94 @@
+// let now = new Date();
+// let s ="hello";
+// console.log(s.toUpperCase());
+// let S =s.toUpperCase();
+// console.log(S.toLowerCase());
+
+// var str = "lorem ipsum is simply dummy";
+
+// str2 = str[0].toUpperCase();
+// let flag = false;
+
+// // console.log(str[0].toUpperCase());
+
+// let result = str[0].toUpperCase() + str.substring(1);
+// // console.log(result);
+
+// for (let i = 0; i < str.length; i++) {
+//   if(str[i] == ' ') {
+//     // console.log("ura probel");
+//     console.log(" ");
+
+//     flag = true;
+//   }else{
+//     if ((flag = true)) {
+//       console.log(result[i].toUpperCase());
+
+//       flag = false;
+//     } else {
+//       console.log(result[i]);
+//     }
+//   }
+// }
+
+// console.log(result)
+
+
+
+var str = "please locate where locate occurs!";
+str2= str[0].toUpperCase();
+let flag = false;
+
+// console.log(str[0].toUpperCase());
+let result = str[0].toUpperCase() + str.substring(1);
+
+// console.log(result);
+
+for (let i = 0; i < result.length; i++) {
+  
+   if(str[i] == ' '){
+    // console.log ("Ура пробел");
+    console.log (" ");
+    flag = true;
+   }else{
+     if(flag == true) {
+      console.log (result[i].toUpperCase());
+      flag = false;
+     }else{
+      console.log (result[i]);
+     }
+   }
+
+}
+
+console.log(result);
+
+// var pos = str.indexOf("locate");
+
+// var pos = str.lastIndexOf("locate");
+
+// console.log(str.substr(20,10));
+
+// console.log(now.getFullYear());
+// now.setFullYear(2020)
+// console.log(now.getFullYear());
+
+// let delim = "and";
+
+// let str = "\"first\"";
+
+// let str = String(0);
+
+// let str ="Hello"
+
+// console.log(typeof str);
+
+// console.log(str[0]);
+
+// console.log(str.charCodeAt(0));
+
+// let str2 ="second";
+// let str3 = str +" "+delim+" "+str2;
+// let str4 = `${str} ${delim} ${str2}`;
+// console.log(str3)
+// console.log(str4)
