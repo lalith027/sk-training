06c80d6ef9a6b0b324b21f219e930d06def39085
Lalit <lalith027@gmail.com>
2021-03-30 18:57:50 +0300

    new

--- Gulp/app/js/script.js
+++ Gulp/app/js/script.js
@@ -35,33 +35,33 @@
 
 
 
-var str = "please locate where locate occurs!";
-str2= str[0].toUpperCase();
-let flag = false;
+// var str = "please locate where locate occurs!";
+// str2= str[0].toUpperCase();
+// let flag = false;
 
 // console.log(str[0].toUpperCase());
-let result = str[0].toUpperCase() + str.substring(1);
+// let result = str[0].toUpperCase() + str.substring(1);
 
 // console.log(result);
 
-for (let i = 0; i < result.length; i++) {
+// for (let i = 0; i < result.length; i++) {
   
-   if(str[i] == ' '){
-    // console.log ("Ура пробел");
-    console.log (" ");
-    flag = true;
-   }else{
-     if(flag == true) {
-      console.log (result[i].toUpperCase());
-      flag = false;
-     }else{
-      console.log (result[i]);
-     }
-   }
+//    if(str[i] == ' '){
+//     // console.log ("Ура пробел");
+//     console.log (" ");
+//     flag = true;
+//    }else{
+//      if(flag == true) {
+//       console.log (result[i].toUpperCase());
+//       flag = false;
+//      }else{
+//       console.log (result[i]);
+//      }
+//    }
 
-}
+// }
 
-console.log(result);
+// console.log(result);
 
 // var pos = str.indexOf("locate");
 
@@ -92,3 +92,41 @@ console.log(result);
 // let str4 = `${str} ${delim} ${str2}`;
 // console.log(str3)
 // console.log(str4)
+
+// цикл for
+
+// var i;
+
+// var arr =[1,2,3],
+
+//     len = arr.length;
+
+// for ( i=10;  i > 0;  i-- ){
+// console.log( i );
+// }
+
+// цикл for in
+
+// var obj ={
+//   propp: 'one',
+//   proppSecond: 'two'
+// }, 
+// propp;
+
+// for (prop in obj){
+//   if (obj.hasOwnProperty(prop)){
+//   console.log(prop + ':' + obj[prop]);}
+// }
+
+// цикл while
+// var k =0
+// while(k <10){
+//   console.log(k++);
+
+// }
+
+// do while
+// var h = 0;
+// do {
+//  console.log(h++);
+// }while(h <1);
