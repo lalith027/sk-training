b60fcf8de4e5c073b4d9b8b2651d00b704d758a6
Lalit <lalith027@gmail.com>
2021-04-01 19:23:05 +0300

    new

--- Gulp/app/js/script.js
+++ Gulp/app/js/script.js
@@ -130,3 +130,40 @@
 // do {
 //  console.log(h++);
 // }while(h <1);
+
+var str = "lorem ipsum is simply dummy";
+str2 =str[0].toUpperCase();
+let flag = false;
+
+// console.log(str[0].toUpperCase());
+let result = str[0].toUpperCase() +str.substring(1,5) + "  "+
+str[6].toUpperCase() +str.substring(7,11) + "  " +
+
+str[12].toUpperCase() +str.substring(13,14) + "  "+
+
+str[15].toUpperCase() +str.substring(16,21) + "  "+
+
+str[22].toUpperCase() +str.substring(23,27) + "  "
+; 
+// console.log(result);
+
+for (let i = 0; i < result.length; i++) {
+
+    if(str[i] == ' '){
+    // console.log ("Ура пробел");
+    // console.log (" ");
+    flag =true;
+    }else{
+    if(flag == true){
+    //  console.log(result[i].toUpperCase());
+     flag = false;
+    }else{
+    // console.log(result[i]);
+    }
+
+   }
+
+    
+}
+
+console.log(result);
