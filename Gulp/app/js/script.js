fabae8e40633cdd124e347e5c8939faa63f5ef79
Lalit <lalith027@gmail.com>
2021-04-01 20:59:37 +0300

    stuff

---
+++ Gulp/app/js/script.js
@@ -0,0 +1,49 @@
+// function compareNum(a,b){
+// if (a > b) return 1;
+// if(a ==b) return 0;
+// if (a < b) return -1;
+// }
+
+// let arr =[1,2,10];
+// arr.sort(compareNum);
+// console.log(arr);
+
+//  написать функцию randomNumber (number), 
+// которая наполняет массив случайными числами от 0 до 100, где number - длина массива
+// проверить в массиве есть число, которое вводится через prompt ()
+
+
+
+
+let arr = ['html', 'css', 'javascript']; 
+let nameLengths = arr.map((name) => name.length);
+console.log(nameLengths);
+
+// let arr = ['html', 'css', 'javascript']; 
+// arr.forEach(function(item, i, arr) 
+// { 
+//     alert( i + ": " + item + " (массив:" + arr + ")" ); });
+
+
+
+// let arr = [1, 2, 3, 4];
+// for (const iterator of arr) {
+//     console.log(iterator);
+// }
+
+
+// let arg = prompt("input your number", "0");
+
+// if (arg != null) {
+//     searchElem(arg);
+// } else {
+//   console.log("Cancel was pressed");
+// }
+
+// function searchElem(arg){
+//  let res = arr.indexOf(+arg);
+// if (res == -1) {
+//     console.log("Num not found");
+//   } else {
+//     console.log(res);
+//   }
