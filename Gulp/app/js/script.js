fd58b33bed8e895dabaacd111343f5959a72f2d2
Lalit <lalith027@gmail.com>
2021-04-06 15:13:22 +0300

    new

---
+++ Gulp/app/js/script.js
@@ -0,0 +1,66 @@
+// console.log(4/6);
+// var x;
+// var y;
+// y=3;
+// x =5+y;
+// x  = 6+7;
+// x = "hello";
+// x=6;
+// x =x+4;
+
+// x +=4;
+// var x;
+// x=7;
+// var x =8;
+// var x =9;
+// var y =5;
+// if(1){
+//     y=7;
+//     console.log(y);
+// }
+// console.log(y);
+
+// let z =5; // {}
+// if(1) {
+//     let z =7;
+//     console.log(z);
+// }
+// console.log(z);
+
+// const a = 9;
+// console.log(a);
+// "use script";   
+// var a = 5;
+// var b = 7;
+// var c;
+// console.log(c)
+
+// console.log(a);
+// var a;
+
+// number
+let a =8;
+console.log(typeof a);
+//number
+let b =8.3;
+console.log(typeof b);
+// string
+let c ="hello";
+console.log(typeof c);
+// NaN
+let d;
+let e;
+console.log(typeof d*e);
+// undefined 
+console.log(typeof d);
+
+//null 
+// boolean
+
+let f = true;
+console.log(typeof f);
+console.log(f);
+
+//object
+let g = [2,3,4];
+console.log(typeof g)
